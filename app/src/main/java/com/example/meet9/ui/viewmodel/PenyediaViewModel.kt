e6b8229877aa99d8f539ffb9bb4de2b9cb62bc53
adandoang <akhdanjzif@gmail.com>
2024-12-16 16:51:33 +0700

    mendaftarkan viewmodel' yang baru dibuat

--- app/src/main/java/com/example/meet9/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/meet9/ui/viewmodel/PenyediaViewModel.kt
@@ -2,6 +2,7 @@ package com.example.meet9.ui.viewmodel
 
 import android.text.Editable.Factory
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.lifecycle.viewmodel.initializer
@@ -13,6 +14,23 @@ object PenyediaViewModel{
     val Factory = viewModelFactory {
         initializer {
             MahasiswaViewModel(
+                krsApp().containerApp.repositoryMhs
+            )
+        }
+        initializer {
+            HomeMhsViewModel(
+                krsApp().containerApp.repositoryMhs
+            )
+        }
+        initializer {
+            DetailMhsViewModel(
+                createSavedStateHandle(),
+                krsApp().containerApp.repositoryMhs
+            )
+        }
+        initializer {
+            UpdateMhsViewModel(
+                createSavedStateHandle(),
                 krsApp().containerApp.repositoryMhs
             )
         }
