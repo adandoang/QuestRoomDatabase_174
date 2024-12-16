194429470f17186d1718e428c103eb38503c1b83
adandoang <akhdanjzif@gmail.com>
2024-12-16 15:31:29 +0700

    metode untuk update info mahasiswa yang di db

--- app/src/main/java/com/example/meet9/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/meet9/data/dao/MahasiswaDao.kt
@@ -2,10 +2,13 @@ package com.example.meet9.data.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Update
 import com.example.meet9.data.entity.Mahasiswa
 
 @Dao
 interface MahasiswaDao {
     @Insert
     suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
+    @Update
+    suspend fun updateMahasiswa(mahasiswa: Mahasiswa)
 }
--- app/src/main/java/com/example/meet9/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/meet9/ui/viewmodel/HomeMhsViewModel.kt
@@ -8,11 +8,15 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 
-class HomeMhsViewModel ()
-
 data class HomeUiState (
     val listMhs: List<Mahasiswa> = listOf(),
     val isLoading: Boolean = false,
     val isError: Boolean = false,
     val errorMessage: String = ""
 )
+
+class HomeMhsViewModel (
+    private val repositoryMhs: RepositoryMhs
+) : ViewModel() {
+    val homeUiState: StateFlow<HomeUiState> = repositoryMhs.getAllMhs()
+}
