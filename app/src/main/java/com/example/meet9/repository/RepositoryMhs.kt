b732c4f111ae41fe319d296132f9fbf604f01f2c
adandoang <akhdanjzif@gmail.com>
2024-12-16 15:38:13 +0700

    metod untuk memanggil fungsi getAllMahasiswa di dao untuk mendapatkan dan dengan aliran data(flow)

--- app/src/main/java/com/example/meet9/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/meet9/repository/RepositoryMhs.kt
@@ -1,7 +1,9 @@
 package com.example.meet9.repository
 
 import com.example.meet9.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
+    fun getAllMhs(): Flow<List<Mahasiswa>>
 }
