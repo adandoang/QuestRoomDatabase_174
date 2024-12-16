fd3e7e9ef3e5084509c3a7bd08f791f22c578a8a
adandoang <akhdanjzif@gmail.com>
2024-12-16 15:43:23 +0700

    metod untuk menghapus data mahasiswa

--- app/src/main/java/com/example/meet9/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/meet9/repository/RepositoryMhs.kt
@@ -5,6 +5,7 @@ import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
+    suspend fun deleteMhs(mahasiswa: Mahasiswa)
     fun getAllMhs(): Flow<List<Mahasiswa>>
     fun getMhs (nim: String): Flow<Mahasiswa>
 }
