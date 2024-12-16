5bc91f48505a5c19cf36ec00d24bd917e2d2fe60
adandoang <akhdanjzif@gmail.com>
2024-12-16 15:39:45 +0700

    metod untuk memanggil fungsi getMahasiswa dari dao untuk mengambil data mahasiswa by nim

--- app/src/main/java/com/example/meet9/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/meet9/repository/RepositoryMhs.kt
@@ -6,4 +6,5 @@ import kotlinx.coroutines.flow.Flow
 interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
     fun getAllMhs(): Flow<List<Mahasiswa>>
+    fun getMhs (nim: String): Flow<Mahasiswa>
 }
