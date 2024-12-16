66deea71b4a71330f968ef6ace36d98f23a89c68
adandoang <akhdanjzif@gmail.com>
2024-12-16 16:19:50 +0700

    membuat class DetailMhsViewModel untuk mendapat data mahasiswa berdasarkan nim dan mengelola state ui, dan membuat fun deleteMhs

--- app/src/main/java/com/example/meet9/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/meet9/ui/viewmodel/DetailMhsViewModel.kt
@@ -3,12 +3,19 @@ package com.example.meet9.ui.viewmodel
 import androidx.core.app.NotificationCompat.MessagingStyle.Message
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.meet9.repository.LocalRepositoryMhs
 import com.example.meet9.repository.RepositoryMhs
 import com.example.meet9.ui.navigation.DestinasiDetail
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
 data class DetailUiState(
     val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
@@ -34,7 +41,36 @@ class DetailMhsViewModel(
         .filterNotNull()
         .map {
             DetailUiState(
-                detailUiEvent =
+                detailUiEvent = it.toDetailUiEvent(),
+                isLoading = false,
             )
         }
+        .onStart {
+            emit(DetailUiState(isLoading = true))
+            delay(600)
+        }
+        .catch {
+            emit(
+                DetailUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(2000),
+            initialValue = DetailUiState(
+                isLoading = true,
+            ),
+        )
+
+    fun deleteMhs() {
+        detailUiState.value.detailUiEvent.toMahasiswaEntity().let {
+            viewModelScope.launch {
+                repositoryMhs.deleteMhs(it)
+            }
+        }
+    }
 }
